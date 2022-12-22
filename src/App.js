23298eda967ee4b8c338ed09b4f14bbb8cd609d5
WilhamsGomes <wilhamsgoomes@gmail.com>
2022-12-21 22:43:47 -0300

    Estudo funções por props e listas

--- src/App.js
+++ src/App.js
@@ -1,32 +1,64 @@
-import React, { Fragment } from "react";
+import React, { useState } from "react";
 import Post from "./Post";
+import Header from "./Header";
 
-const tituloZeroUm = "Titulo Zero UM";
+
 
 function App(){
+
+    const [ posts, setPosts ] = useState([
+        {id: Math.random(), title: "Title #01", subtitle: "Sub#01", likes: 20},
+        {id: Math.random(), title: "Title #02", subtitle: "Sub#02", likes: 30},
+        {id: Math.random(), title: "Title #03", subtitle: "Sub#03", likes: 25},
+        {id: Math.random(), title: "Title #04", subtitle: "Sub#04", likes: 45},
+    ]);
+
+
+    console.log({posts})
+
+    function handleRefresh(){
+        //Atualiza o estado e depende de valores que estão nele
+        setPosts( (prevState) => [
+            ...prevState, 
+            {
+                id: Math.random(), 
+                title: `Title#0${prevState.length + 1}`, 
+                subtitle: `Sub#0${prevState.length + 1}`, 
+                likes: 45
+            }
+        ] );
+    }
+
+    function handleRemovePost(postId){
+        setPosts((prevState) => (
+            prevState.filter(post => post.id !== postId)
+        ));
+    }
+
     return(
         <>
-            <h1>JStack's Blog </h1>
-            <h2>Posts da semana</h2>
+            <Header title="Jornal do comércio">
+                <h2>
+                    Posts da semana sobre esportes
+                    <button onClick={handleRefresh}>Atualizar</button>    
+                </h2> 
+            </Header>
+
             <hr/>
-            <Post 
-                post={{
-                    title:"Titulo da noticia 01",
-                    subtitle: "Subtitulo da noticia 01",
-                }}
-            />
-            <Post 
-                post={{
-                    title:"Titulo da noticia 02",
-                    subtitle: "Subtitulo da noticia 02",
-                }}
-            />
-            <Post 
-                post={{
-                    title:"Titulo da noticia 03",
-                    subtitle: "Subtitulo da noticia 03",
-                }}
-            />
+            
+            {posts.map(post => (
+                <Post 
+                    key={post.id}
+                    likes={post.likes}
+                    onRemove={handleRemovePost}
+                    post={{
+                        id: post.id,
+                        title: post.title,
+                        subtitle: post.subtitle,
+                    }}
+                />
+            ))}
+
         </>
     );
 }
--- src/Post.js
+++ src/Post.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropsTypes from 'prop-types';
 
 export default function Post(props){
 
@@ -6,10 +7,25 @@ export default function Post(props){
         <>
             <article>
                 <strong>{props.post.title}</strong>
+                <button onClick={ () => props.onRemove(props.post.id)}>
+                    Remover
+                </button>
                 <br />
                 <small>{props.post.subtitle}</small>
+                <br/>
+                Likes: {props.likes / 2}
             </article>
             <br />
         </>
     );
 }
+
+Post.propTypes = {
+    likes: PropsTypes.number.isRequired,
+    onRemove: PropsTypes.func.isRequired,
+    post: PropsTypes.shape({
+        id: PropsTypes.number.isRequired,
+        title: PropsTypes.string.isRequired,
+        subtitle: PropsTypes.string.isRequired,
+    }).isRequired,
+};
